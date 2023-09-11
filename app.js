1f37e54c736a028870b94aa77b49888b69349364
warith <warith.ad02@gmail.com>
2023-09-11 03:04:56 +0100

    Edit start script

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ const logger = require('morgan')
 const app = express()
 const PORT = process.env.PORT || 3030
 
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }));
 app.use(logger('dev'))
+
 
 app.use('/api', (req, res, next) => {
     const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thurday', 'Friday', 'Saturday']
