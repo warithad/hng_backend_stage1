94c6081765ff3c5ba713b30e571cda7ff0b05cb0
warith <warith.ad02@gmail.com>
2023-09-11 02:37:04 +0100

    Complete app.js

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
-var express = require('express')
-var path = require('path')
+const express = require('express')
+const path = require('path')
+const logger = require('morgan')
 const app = express()
 const PORT = process.env.PORT || 3030
 
-app.use('/api/', (req, res, next) => {
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }));
+app.use(logger('dev'))
+
+app.use('/api', (req, res, next) => {
     const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thurday', 'Friday', 'Saturday']
     const date = new Date()
 
     const current_day = weekDays[date.getDay()]
     const utc_time = date.toUTCString()
 
-    const slack_name = req.params.slack_name 
-    const track = req.params.track
-    const github_file_url = ''
+    const slack_name = req.query.slack_name 
+    const track = req.query.track
+    const github_file_url = 'https://github.com/warithad/hng_backend_stage1/blob/main/app.js'
     const github_repo_url = 'https://github.com/warithad/hng_backend_stage1'
 
     res.status(200).json({
@@ -25,7 +30,7 @@ app.use('/api/', (req, res, next) => {
         status_code: 200
     })
 })
-
+  
 app.listen(PORT, () => {
     console.log(`listening on PORT: ${PORT}`)
 })
